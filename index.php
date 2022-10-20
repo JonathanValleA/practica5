ed69daf7e698101fd88a45fa576c69b4a14caaaf
JonathanValleA <jvalle@jaumebalmes.net>
2022-10-20 20:34:29 +0200

    canvis en index

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ try {
            <td><?php echo $product['Name'] ?></td> <!--Accedim a Name-->
            <td><?php echo $product['Descripcion'] ?></td> <!--Accedim a Description-->
            <td><?php echo $product['Price'] ?></td> <!--Accedim a Price -->
-           <td><a href="edit.php?id=<?php echo $product['id']?>"><button type="button" class="btn btn-outline-primary">Edit</button></a></td>
+           <td><a href="plantilla.php?id=<?php echo $product['id']?>"><button type="button" class="btn btn-outline-primary">Edit</button></a></td>
            <td><a href="delete.php?id=<?php echo $product['id']?>"><button type="button" class="btn btn-outline-danger">Delete</button></a></td>
        </tr>
    <?php } ?>
@@ -51,7 +51,7 @@ try {
 			<div class="col-md-4">
 				<div class="card card-body">
 				<!-- A través del mètode POST li enviem les dades del formulari a l'arxiu add_product.php -->
-					<form action="añadir_producto.php" method="POST"> 
+					<form action="añadir.php" method="POST"> 
 						<div class=form-group>
 							<input type="text" name="name" class="form-control" placeholder="Name" autofocus>
 						</div>
@@ -60,9 +60,6 @@ try {
 						</div>
 						<div class=form-group>
 							<input type="text" name="price" class="form-control" placeholder="price">
-						</div>
-						<div class=form-group>
-							<input type="text" name="quantity" class="form-control" placeholder="Quantity">
 						</div>
 						<input type="submit" class="btn btn-success btn-block" name="add_product" value="+ Producte">
 					</form>
