91871346d10f23c879fa186321e167387c9aeaad
Reymon <102548004+ReymonVega@users.noreply.github.com>
2022-11-03 19:06:21 +0100

    Actualizacion Main - Rama (#4)
    * edit.php
    
    * canvis en index
    
    * Index.php y conexion
    
    Co-authored-by: jgimenezbalmes <jgimenez@jaumebalmes.net>
    Co-authored-by: JonathanValleA <jvalle@jaumebalmes.net>
    Co-authored-by: jgimenezbalmes <113448133+jgimenezbalmes@users.noreply.github.com>
    Co-authored-by: JonathanValleA <113449282+JonathanValleA@users.noreply.github.com>

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ try {
 			<div class="col-md-4">
 				<div class="card card-body">
 				<!-- A través del mètode POST li enviem les dades del formulari a l'arxiu add_product.php -->
-					<form action="añadir_producto.php" method="POST"> 
+					<form action="añadir.php" method="POST"> 
 						<div class=form-group>
 							<input type="text" name="name" class="form-control" placeholder="Name" autofocus>
 						</div>
@@ -60,9 +60,6 @@ try {
 						</div>
 						<div class=form-group>
 							<input type="text" name="price" class="form-control" placeholder="price">
-						</div>
-						<div class=form-group>
-							<input type="text" name="quantity" class="form-control" placeholder="Quantity">
 						</div>
 						<input type="submit" class="btn btn-success btn-block" name="add_product" value="+ Producte">
 					</form>
